811b4c74d7c29347f510509df243b7ce94983b96
Iamneo <techadmin@iamneo.ai>
2023-11-23 17:34:36 +0000

    Auto-Commit-At-23/11/2023--23:04:34

---
+++ app.py
@@ -0,0 +1,31 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import time
+
+def run_test_on_remote_browser():
+    # Specify the remote server URL
+    remote_url = "http://34.85.201.58:4498"
+
+    # Set up Chrome options
+    chrome_options = Options()
+    # Add any specific options you want
+    # For example, to run headless: chrome_options.add_argument("--headless")
+
+    # Initialize the remote WebDriver with the updated argument
+    driver = webdriver.Remote(command_executor=remote_url, options=chrome_options)
+
+    try:
+        # Navigate to a website
+        driver.get("https://www.gillette.co.in")
+        time.sleep(15)  # Waiting for the page to load
+        print("Test completed successfully")
+
+    except Exception as e:
+        print(f"Test failed: {e}")
+
+    finally:
+        # Close the browser
+        driver.quit()
+
+if __name__ == "__main__":
+    run_test_on_remote_browser()
---
+++ demo/src/test/python/exception/eventhandler.py
@@ -0,0 +1,14 @@
+import os
+import logging
+
+class EventHandler:
+    def __init__(self):
+        pass
+
+    def handle_event(self, event):
+
+        pass
+
+    def log_event(self, event):
+
+        pass
