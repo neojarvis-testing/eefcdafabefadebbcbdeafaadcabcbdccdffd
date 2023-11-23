d569d6a1aa7a4e6fc7d39c4b21770b22dcea9937
Iamneo <techadmin@iamneo.ai>
2023-11-23 18:20:41 +0000

    Open-Project-At-23/11/2023--23:50:39

--- app.py
+++ app.py
@@ -1,31 +1,81 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-import time
+import os
+import logging
+from selenium.webdriver.common.by import By
+# from selenium.webdriver.support.events import 
+from selenium.webdriver.support.abstract_event_listener import AbstractEventListener
+from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.remote.webelement import WebElement
 
-def run_test_on_remote_browser():
-    # Specify the remote server URL
-    remote_url = "http://34.85.201.58:4499"
+def configure_logger(log_file_path):
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+    file_handler = logging.FileHandler(log_file_path)
+    file_handler.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(message)s')
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+    return logger
 
-    # Set up Chrome options
-    chrome_options = Options()
-    # Add any specific options you want
-    # For example, to run headless: chrome_options.add_argument("--headless")
+class EventHandler(AbstractEventListener):
+    def __init__(self):
+        log_dir = "/home/coder/project" 
+        if not os.path.exists(log_dir):
+            os.makedirs(log_dir)
 
-    # Initialize the remote WebDriver with the updated argument
-    driver = webdriver.Remote(command_executor=remote_url, options=chrome_options)
+        log_file_path = os.path.join(log_dir, "log.log")
+        self.logger = configure_logger(log_file_path)
+        self.logged_events = set()
 
-    try:
-        # Navigate to a website
-        driver.get("https://www.gillette.co.in")
-        time.sleep(15)  # Waiting for the page to load
-        print("Test completed successfully")
+    def _log_event(self, event_text):
+        if event_text not in self.logged_events:
+            self.logged_events.add(event_text)
+            self.logger.info(event_text)
 
-    except Exception as e:
-        print(f"Test failed: {e}")
+    def before_click(self, element: WebElement, driver: WebDriver):
+        element_text = element.text
+        element_value = element.get_attribute('value')
 
-    finally:
-        # Close the browser
-        driver.quit()
+        if element_text is None:
+            event_text = f"Clicked {element_value}"
+        elif element_value is None:
+            event_text = f"Clicked {element_text}"
+        else:
+            event_text = f"Clicked {element_text}{element_value}"
 
-if __name__ == "__main__":
-    run_test_on_remote_browser()
+        self._log_event(event_text)
+
+    def before_find(self, by, value, driver):
+        if by == By.XPATH:
+            if "//" in value or "/html" in value or "//html" in value:
+                element = driver.find_element(By.XPATH, value)
+                element_name = element.text.strip()
+                if not element_name:
+                    element_name = element.get_attribute("name") or element.get_attribute("id") or element.get_attribute("class") or "unknown_element"
+                value = f"{value}[contains(text(), '{element_name}')]"
+                event_text = f"{element_name} #absolute"
+            else:
+                element = driver.find_element(By.XPATH, value)
+                element_name = element.text.strip()
+                event_text = element_name
+
+        self._log_event(event_text)
+
+    def before_send_keys(self, element: WebElement, keys_to_send):
+        element_info = self._get_xpath_info(element)
+        self.logger.info(f"Sending keys to {element_info}: {keys_to_send}")
+
+    def _get_xpath_info(self, element: WebElement):
+        xpath = element.get_attribute("xpath")
+        element_id = element.get_attribute("id")
+        element_name = element.get_attribute("name")
+        element_class = element.get_attribute("class")
+        if xpath:
+            return xpath
+        elif element_id:
+            return f"element with id='{element_id}'"
+        elif element_name:
+            return f"element with name='{element_name}'"
+        elif element_class:
+            return f"element with class='{element_class}'"
+        else:
+            return "unknown element"
